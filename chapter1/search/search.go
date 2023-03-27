aafb1cac3ddbced6886c5a1d9ad2500d66a18a37
hezefeng <hzf1998@126.com>
2023-03-27 15:20:53 +0800

    update..

--- chapter1/search/search.go
+++ chapter1/search/search.go
@@ -1,6 +1,10 @@
 package search
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+)
 
 //var matchers = make(map[string]Matcher)
 
@@ -9,5 +13,25 @@ func init() {
 }
 
 func Run() {
+
+	var waitGroup sync.WaitGroup
+	myarr := []int{1, 2, 3, 4, 5, 6}
+	waitGroup.Add(len(myarr))
+
+	for _, v := range myarr {
+		go func(v int) {
+			res, _ := calculation(v)
+			fmt.Println(res)
+			waitGroup.Done()
+		}(v)
+	}
+
+	waitGroup.Wait()
+
 	fmt.Println("this is search.Run")
+}
+
+func calculation(number int) (int, error) {
+	res := number + rand.Int()
+	return res, nil
 }
